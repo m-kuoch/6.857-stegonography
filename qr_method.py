ad738cca60168543fa41cb8e66314e940a74170d
m-kuoch <m.kuoch1@gmail.com>
2022-04-28 18:12:31 -0400

    another DWT method, doesn't work too well...

--- qr_method.py
+++ qr_method.py
@@ -4,13 +4,21 @@ from PIL import Image
 import pywt
 
 if __name__ == '__main__':
-    fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(16, 8))
+    fig, axes = plt.subplots(nrows=3, ncols=4, figsize=(12, 9))
     im = Image.open('lady.jpg').convert('L')
     cover = np.array(im)  # 2D array
     secret = np.array(Image.open('secret_grayscale.jpg').convert('L'))
 
     cover = cover / 255
     secret = secret / 255
+
+    # Pad asymmetrically with zeros
+    cover = np.concatenate((cover, np.zeros((cover.shape[0], 1))), axis=1)
+    cover = np.concatenate((cover, np.zeros((1, cover.shape[1]))), axis=0)
+    secret = np.concatenate((secret, np.zeros((secret.shape[0], 1))), axis=1)
+    secret = np.concatenate((secret, np.zeros((1, secret.shape[1]))), axis=0)
+
+    print(cover.shape, secret.shape)
 
     def qr_hide(cover, secret, axes, row=0):
         """QR method with no DWT"""
@@ -27,7 +35,6 @@ if __name__ == '__main__':
         # Combine cover and secret, generate stego
         r_combined = rc + (alpha * rs)
         stego = qc @ r_combined
-        print(stego)
         axes[row][2].imshow(np.uint8(stego*255), cmap='gray', vmin=0, vmax=255)
         axes[row][2].set_title('Stego')
 
@@ -42,7 +49,7 @@ if __name__ == '__main__':
 
     def qr_hide_dwt(cover, secret, axes, row=0):
         """QR method with DWT"""
-        alpha = 0.1
+        alpha = 0.01
 
         axes[row][0].imshow(cover*255, cmap='gray', vmin=0, vmax=255)
         axes[row][0].set_title('Cover')
@@ -73,6 +80,37 @@ if __name__ == '__main__':
         axes[row][3].imshow(np.uint8(recovered*255), cmap='gray', vmin=0, vmax=255)
         axes[row][3].set_title('Recovered')
     qr_hide_dwt(cover, secret, axes, row=1)
+
+    def qr_hide_dwt2(cover, secret, axes, row=0):
+        """QR method with DWT after QR decomposition (histo paper)"""
+        alpha = 0.01
+
+        axes[row][0].imshow(cover*255, cmap='gray', vmin=0, vmax=255)
+        axes[row][0].set_title('Cover')
+        axes[row][1].imshow(secret*255, cmap='gray', vmin=0, vmax=255)
+        axes[row][1].set_title('Secret')
+
+        # QR decomposition of secreet image
+        qs, rs = np.linalg.qr(secret)
+
+        # DWT of cover image and QR decomposition of secret image
+        LL_secret, other_secret = pywt.dwt2(secret, 'coif1')
+        LL_cover, other_cover = pywt.dwt2(cover, 'coif1')
+
+        # Combine cover and secret, generate stego
+        r_combined = LL_cover + (alpha * LL_secret)
+        stego = pywt.idwt2((r_combined, other_cover), 'coif1')
+        axes[row][2].imshow(np.uint8(stego*255), cmap='gray', vmin=0, vmax=255)
+        axes[row][2].set_title('Stego')
+
+        # Extract secret image
+        rsi, other_recovered = pywt.dwt2(stego, 'coif1')
+        r_extracted = (rsi - LL_cover) / alpha
+        r_extracted = pywt.idwt2((r_extracted, other_secret), 'coif1')
+        recovered = qs @ r_extracted
+        axes[row][3].imshow(np.uint8(recovered*255), cmap='gray', vmin=0, vmax=255)
+        axes[row][3].set_title('Recovered')
+    qr_hide_dwt2(cover, secret, axes, row=2)
 
     plt.show()
 

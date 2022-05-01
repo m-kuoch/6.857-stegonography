b0ba45e78a57dd1e15dd634f5290e7fc01e6b225
Enrique <enriquec@mit.edu>
2022-05-01 18:16:22 -0400

    Added run_comparison function

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import stega
+import pipeline as p
 
 
 def main() -> None:
-    stega.stegano_hide("./lady.png", "hello world", "breuh.png")
-    print(stega.reveal("breuh.png"))
-    pass
+    # stega.stegano_hide("./lady.png", "hello world", "breuh.png")
+    # print(stega.reveal("breuh.png"))
+    evaluation = p.run_comparison(
+        "lady.jpg",
+        "secret_grayscale.jpg",
+        ["qr", "dwt_qr", "fwt_qr", "qr_dwt", "qr_fwt"],
+        exe_config={"qr_dwt": {"wavelet": "db1"}},
+        display_config={
+            "qr": {"label": "QR Method Only"},
+            "dwt_qr": {"label": "QR Method with DWT"},
+            "fwt_qr": {"label": "QR with FFT2"},
+        },
+    )
+    print(evaluation)
 
 
 if __name__ == "__main__":
--- pipeline.py
+++ pipeline.py
@@ -1,59 +1,20 @@
 from matplotlib import pyplot as plt
-import numpy as np
+import evaluation as eval
 from PIL import Image
+import numpy as np
 import pywt
 
 
-def load_image(path):
+def _load_image(path):
     return np.array(Image.open(path).convert("L"))
 
 
-def show_image(image):
+def _show_image(image):
     plt.imshow(image, cmap="gray", vmin=0, vmax=255)
     plt.show()
 
 
-def qr(image):
-    qs, rs = np.linalg.qr(image)
-    return qs, rs
-
-
-def dwt(image, wavelet="db1"):
-    LL, other_cover = pywt.dwt2(image, wavelet)
-    return LL, other_cover
-
-def stego_pipeline(cover_path, secret_path, method: function, row):
-    fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(12, 12))
-    
-    cover = load_image(cover_path)
-    secret = load_image(secret_path)
-
-    # Preprocess images (scale to 0-1, pad asymmetrically with zeros)
-    cover = cover / 255
-    secret = secret / 255
-    cover = np.concatenate((cover, np.zeros((cover.shape[0], 1))), axis=1)
-    cover = np.concatenate((cover, np.zeros((1, cover.shape[1]))), axis=0)
-    secret = np.concatenate((secret, np.zeros((secret.shape[0], 1))), axis=1)
-    secret = np.concatenate((secret, np.zeros((1, secret.shape[1]))), axis=0)
-    
-    # Show the cover image
-    axes[row][0].imshow(cover*255, cmap='gray', vmin=0, vmax=255)
-    axes[row][0].set_title('Cover')
-    
-    # Show the secret image
-    axes[row][1].imshow(secret*255, cmap='gray', vmin=0, vmax=255)
-    axes[row][1].set_title('Secret')
-    
-    stego, recovered = method(cover, secret)
-
-    # Get the stego image
-    axes[row][2].imshow(np.uint8(stego*255), cmap='gray', vmin=0, vmax=255)
-    axes[row][2].set_title('Stego')
-    axes[row][3].imshow(np.uint8(recovered*255), cmap='gray', vmin=0, vmax=255)
-    axes[row][3].set_title('Recovered')
-
-
-def qr_hide(cover, secret, alpha):
+def qr_only(cover, secret, alpha=0.01):
     qc, rc = np.linalg.qr(cover)
     qs, rs = np.linalg.qr(secret)
 
@@ -69,4 +30,177 @@ def qr_hide(cover, secret, alpha):
     return stego, recovered
 
 
+def dwt_qr(cover, secret, alpha=0.01, wavelet="db1"):
+    """QR method with Discrete Wavelet Transform (DWT)"""
+    LL, other_cover = pywt.dwt2(cover, wavelet)
+    qc, rc = np.linalg.qr(LL)
 
+    LL_secret, other_secret = pywt.dwt2(secret, wavelet)
+    qs, rs = np.linalg.qr(LL_secret)
+
+    # Combine cover and secret, generate stego
+    r_combined = rc + (alpha * rs)
+    stego = qc @ r_combined
+    stego = pywt.idwt2((stego, other_cover), wavelet)
+
+    # Extract secret image
+    LL, other = pywt.dwt2(stego, wavelet)
+    qsi, rsi = np.linalg.qr(LL)
+    r_extracted = (rsi - rc) / alpha
+    recovered = qs @ r_extracted
+    recovered = pywt.idwt2((recovered, other_secret), wavelet)
+
+    return stego, recovered
+
+
+def fwt_qr(cover, secret, alpha=0.01):
+    """QR method with Discrete FOurier Transform (DFT)"""
+    LL = np.fft.fft2(cover)
+    qc, rc = np.linalg.qr(LL)
+
+    LL_secret = np.fft.fft2(secret)
+    qs, rs = np.linalg.qr(LL_secret)
+
+    # Combine cover and secret, generate stego
+    r_combined = rc + (alpha * rs)
+    stego = qc @ r_combined
+    stego = np.fft.ifft2(stego)
+
+    # Extract secret image
+    LL = np.fft.fft2(stego)
+    qsi, rsi = np.linalg.qr(LL)
+    r_extracted = (rsi - rc) / alpha
+    recovered = qs @ r_extracted
+    recovered = np.fft.ifft2(recovered)
+
+    return stego, recovered
+
+
+def qr_dwt(cover, secret, alpha=0.01, wavelet="db1"):
+    """QR method with DWT after QR decomposition (histo paper)"""
+    # QR decomposition of secret image
+    qs, rs = np.linalg.qr(secret)
+
+    # DWT of cover image and QR decomposition of secret image
+    LL_secret, other_secret = pywt.dwt2(rs, wavelet)
+    LL_cover, other_cover = pywt.dwt2(cover, wavelet)
+
+    # Combine cover and secret, generate stego
+    r_combined = LL_cover + (alpha * LL_secret)
+
+    other_combined = []
+    for i in range(len(other_secret)):
+        other_combined.append(other_cover[i] + (alpha * other_secret[i]))
+    other_combined = tuple(other_combined)
+
+    stego = pywt.idwt2(
+        (r_combined, other_combined), wavelet
+    )  # replace other_combined with other_cover
+
+    # Extract secret image
+    rsi, other_recovered = pywt.dwt2(stego, wavelet)
+    r_extracted = (rsi - LL_cover) / alpha
+    r_extracted = pywt.idwt2((r_extracted, other_secret), wavelet)
+    recovered = qs @ r_extracted
+
+    return stego, recovered
+
+
+def qr_fwt(cover, secret, alpha=0.01):
+    """QR method with DWT after QR decomposition (histo paper)"""
+    # QR decomposition of secret image
+    qs, rs = np.linalg.qr(secret)
+
+    # DWT of cover image and QR decomposition of secret image
+    LL_secret = np.fft.fft2(rs)
+    LL_cover = np.fft.fft2(cover)
+
+    # Combine cover and secret, generate stego
+    r_combined = LL_cover + (alpha * LL_secret)
+
+    stego = np.fft.ifft2(r_combined)
+
+    # Extract secret image
+    rsi = np.fft.fft2(stego)
+    r_extracted = (rsi - LL_cover) / alpha
+    r_extracted = np.fft.ifft2(r_extracted)
+    recovered = qs @ r_extracted
+
+    return stego, recovered
+
+
+METHODS_MAP = {
+    "qr": qr_only,
+    "dwt_qr": dwt_qr,
+    "fwt_qr": fwt_qr,
+    "qr_dwt": qr_dwt,
+    "qr_fwt": qr_fwt,
+}
+
+
+def run_comparison(
+    cover_path: str,
+    secret_path: str,
+    methods,
+    exe_config: dict = {},
+    display_config: dict = {},
+):
+    evaluations = {}
+
+    # Preprocess images (scale to 0-1, pad asymmetrically with zeros)
+    cover = _load_image(cover_path)
+    secret = _load_image(secret_path)
+    cover = cover / 255
+    secret = secret / 255
+    cover = np.concatenate((cover, np.zeros((cover.shape[0], 1))), axis=1)
+    cover = np.concatenate((cover, np.zeros((1, cover.shape[1]))), axis=0)
+    secret = np.concatenate((secret, np.zeros((secret.shape[0], 1))), axis=1)
+    secret = np.concatenate((secret, np.zeros((1, secret.shape[1]))), axis=0)
+
+    num_methods = len(methods)
+    fig, axes = plt.subplots(nrows=num_methods, ncols=4, figsize=(12, 12))
+
+    for i, method in enumerate(methods):
+        method_func = METHODS_MAP[method]
+        execution_config = exe_config.get(method, {})
+        stego, recovered = method_func(cover, secret, **execution_config)
+
+        cover_display = np.uint8(cover * 255)
+        secret_display = np.uint8(secret * 255)
+        stego_display = np.uint8(stego * 255)
+        recovered_display = np.uint8(recovered * 255)
+
+        # Show the cover image
+        axes[i][0].imshow(cover_display, cmap="gray", vmin=0, vmax=255)
+        axes[i][0].set_title("Cover")
+
+        # Show the secret image
+        axes[i][1].imshow(secret_display, cmap="gray", vmin=0, vmax=255)
+        axes[i][1].set_title("Secret")
+
+        # Show the stego image
+        axes[i][2].imshow(stego_display, cmap="gray", vmin=0, vmax=255)
+        axes[i][2].set_title("Stego")
+
+        # Show the recovered image
+        axes[i][3].imshow(recovered_display, cmap="gray", vmin=0, vmax=255)
+        axes[i][3].set_title("Recovered")
+
+        try:
+            label = display_config[method]["label"]
+        except KeyError:
+            label = method
+        axes[i][0].set_ylabel(label)
+
+        evaluation_dict = eval.evaluate_images(
+            cover_display,
+            secret_display,
+            stego_display,
+            recovered_display,
+        )
+        evaluations[method] = evaluation_dict
+        print('Evaluation for "{}":'.format(method))
+        print(evaluation_dict)
+
+    plt.show()
+    return evaluations

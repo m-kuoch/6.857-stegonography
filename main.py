a8ec31ccf423b94ccfeea994b1587b337bdff78d
Enrique <enriquec@mit.edu>
2022-05-02 15:42:29 -0400

    I fixed the evaluation in main Thanh

--- main.py
+++ main.py
@@ -11,25 +11,35 @@ import dataframe_image as dfi
 def main() -> None:
     # stega.stegano_hide("./lady.png", "hello world", "breuh.png")
     # print(stega.reveal("breuh.png"))
+    # evaluation = p.run_comparison(
+    #     cover_path=os.path.join("images", "cover", "lady.jpg"),
+    #     secret_path=os.path.join("images", "secret", "secret_grayscale.jpg"),
+    #     methods=[
+    #         {
+    #             "name": "qr",
+    #             "label": "QR Method only (alpha 0.05)",
+    #             "exe_config": {"alpha": 0.05},
+    #         },
+    #         {
+    #             "name": "qr",
+    #             "label": "QR Method only (default alpha)",
+    #         },
+    #     ],
+    # )
     evaluation = p.run_comparison(
         cover_path=os.path.join("images", "cover", "lady.jpg"),
         secret_path=os.path.join("images", "secret", "secret_grayscale.jpg"),
         methods=[
-            {
-                "name": "qr",
-                "label": "QR Method only (alpha 0.05)",
-                "exe_config": {"alpha": 0.05},
-            },
-            {
-                "name": "qr",
-                "label": "QR Method only (default alpha)",
-            },
+            "qr", "dwt_qr", "fwt_qr", "qr_dwt", "qr_fwt",
         ],
     )
+    print(evaluation)
+    df = pd.DataFrame(evaluation)
+    # dfi.save_dataframe_image(df, "evaluation2.png")
     df = pd.DataFrame.from_dict(evaluation, orient='index')
     # df.style.apply(highlight_max).to_html("results.html")
     df_styled = df.style.highlight_max(color='lightgreen', axis=0).highlight_min(color='lightcoral', axis=0)
-    dfi.export(df_styled, 'dataframe.png')
+    dfi.export(df_styled, 'dataframe2.png')
     print(df)
     print(evaluation)
 
